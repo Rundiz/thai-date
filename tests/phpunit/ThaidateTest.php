a29e1f5883a59b699721ad6c491fcabd8a1e441c
vee w <kawewong@gmail.com>
2016-04-22 16:19:44 +0700

    fix test to support thai locale

--- tests/phpunit/ThaidateTest.php
+++ tests/phpunit/ThaidateTest.php
@@ -12,6 +12,7 @@ class ThaidateTest extends \PHPUnit_Framework_TestCase
         $timestamp = 1460619637;
 
         $this->assertEquals('วันพฤหัสบดีที่ 14 เมษายน พ.ศ.2559', thaidate('วันlที่ j F พ.ศ.Y', $timestamp));
+        $this->assertEquals('พฤ. 14 เม.ย. 59', thaistrftime('%a %d %b %y', $timestamp, true, array('th', 'th_TH.utf8', 'th_TH.UTF8', 'th_TH.utf-8', 'th_TH.UTF-8', 'th_TH', 'th-TH')));
     }// testThaidate
 
 
@@ -21,6 +22,7 @@ class ThaidateTest extends \PHPUnit_Framework_TestCase
         $Thaidate = new \Rundiz\Thaidate\Thaidate();
 
         $this->assertEquals('วันพฤหัสบดีที่ 14 เมษายน พ.ศ.2559', $Thaidate->date('วันlที่ j F พ.ศ.Y', $timestamp));
+        $this->assertEquals('พฤ. 14 เม.ย. 59', $Thaidate->strftime('%a %d %b %y', $timestamp, true, array('th', 'th_TH.utf8', 'th_TH.UTF8', 'th_TH.utf-8', 'th_TH.UTF-8', 'th_TH', 'th-TH')));
     }// testThaidateClass
 
 
