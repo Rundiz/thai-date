4be5939c5d8d648f1e6c368f29b2ffd317595120
ve3 <kawewong@gmail.com>
2021-12-17 05:59:52 +0700

    Add `thaiIntlDate()` function.

--- Rundiz/Thaidate/thaidate-functions.php
+++ Rundiz/Thaidate/thaidate-functions.php
@@ -11,8 +11,8 @@
  * Thai date use date() function.
  * 
  * @param string $format The format as same as PHP date function format. See http://php.net/manual/en/function.date.php
- * @param integer $timestamp The optional timestamp is an integer Unix timestamp.
- * @param boolean $buddhist_era Use Buddhist era? set to true to use that or false not to use.
+ * @param int $timestamp The optional timestamp is an integer Unix timestamp.
+ * @param bool $buddhist_era Use Buddhist era? set to true to use that or false not to use.
  * @return string Return the formatted date/time string.
  */
 function thaidate($format, $timestamp = '', $buddhist_era = true)
@@ -28,11 +28,37 @@ function thaidate($format, $timestamp = '', $buddhist_era = true)
 
 
 /**
+ * Thai date use IntlDateFormatter() class.
+ * 
+ * @param string $format The format or pattern as **same** as ICU format. See https://unicode-org.github.io/icu/userguide/format_parse/datetime/
+ * @param type $timestamp The optional timestamp is an integer Unix timestamp.
+ * @param bool $buddhist_era Use Buddhist era? set to true to use that or false not to use.
+ * @param array|string $locale The locale that will be use in `IntlDateFormatter::__construct()` function. See https://www.php.net/manual/en/intldateformatter.create.php
+ * @return string Return the formatted date/time string.
+ */
+function thaiIntlDate($format, $timestamp = '', $buddhist_era = true, $locale = 'th')
+{
+    if ($locale == null) {
+        $locale = 'th';
+    }
+
+    if (!is_bool($buddhist_era)) {
+        $buddhist_era = true;
+    }
+
+    $thaidate = new \Rundiz\Thaidate\Thaidate();
+    $thaidate->buddhist_era = $buddhist_era;
+    $thaidate->locale = $locale;
+    return $thaidate->intlDate($format, $timestamp);
+}// thaiIntlDate
+
+
+/**
  * Thai date use strftime() function.
  * 
  * @param string $format The format as same as PHP date function format. See http://php.net/manual/en/function.strftime.php
- * @param integer $timestamp The optional timestamp is an integer Unix timestamp.
- * @param boolean $buddhist_era Use Buddhist era? set to true to use that or false not to use.
+ * @param int $timestamp The optional timestamp is an integer Unix timestamp.
+ * @param bool $buddhist_era Use Buddhist era? set to true to use that or false not to use.
  * @param array|string $locale The locale that will be use in setlocale() function. See http://php.net/setlocale
  * @return string Return the formatted date/time string.
  */
---
+++ tests/via-http/thaiintldate-function.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * This is test file. You can remove this.
+ */
+
+require dirname(dirname(__DIR__)).'/Rundiz/Thaidate/Thaidate.php';
+require dirname(dirname(__DIR__)).'/Rundiz/Thaidate/thaidate-functions.php';
+
+
+header('Content-Type: text/html; charset=UTF-8');
+echo '<meta charset="utf-8">' . PHP_EOL;
+
+echo 'Begin test thaistrftime();.'."<br>\n";
+echo 'time(); = '.time()."<br>\n";
+echo 'Current date/time use date(); = '.date('Y-m-d H:i:s')."<br>\n";
+if (function_exists('strftime')) {
+    echo 'Current date/time use strftime(); = ' . @strftime('%#d %A %B %Y')."<br>\n";
+    if (version_compare(PHP_VERSION, '8.1', '>=')) {
+        echo '<div style="border-left: 5px solid #f00; box-shadow: 0 1px 3px rgba(0, 0, 0, 0.6); font-size: 1em; margin: 0 0 20px; padding: 2px 5px;"><code>strftime()</code> is deprecated since PHP 8.1.</div>' . PHP_EOL;
+    }
+} else {
+    echo 'Function strftime() is not exists.<br>' . "\n";
+}
+if (class_exists('IntlDateFormatter')) {
+    $IntlDateFormatter = new \IntlDateFormatter(null, \IntlDateFormatter::FULL, \IntlDateFormatter::FULL);
+    echo 'Current date/time use IntlDateFormatter(); = ' . $IntlDateFormatter->format(time()) . "<br>\n";
+} else {
+    echo 'Class IntlDateFormatter() is not exits.<br>' . "\n";
+}
+echo '----------------------------------'."<br>\n";
+echo 'Thai date test.'."<br>\n";
+echo '12 Months'."<br>\n";
+for ($i = 1; $i <= 12; $i++) {
+    echo thaistrftime('%A %d %B %Y', strtotime(date('Y').'-'.$i.'-01'))."<br>\n";
+}
+echo '---------------------'."<br>\n";
+echo '12 Months in short'."<br>\n";
+for ($i = 1; $i <= 12; $i++) {
+    echo thaistrftime('%a %d %b %y', strtotime(date('Y').'-'.$i.'-01'))."<br>\n";
+}
+echo '---------------------'."<br>\n";
+echo 'Full long date with time'."<br>\n";
+echo sprintf(thaistrftime('%%s%A%%s %d %B %%s%Y %%s%H:%M:%S'), 'วัน', 'ที่', 'พ.ศ.', 'เวลา')."<br>\n";
